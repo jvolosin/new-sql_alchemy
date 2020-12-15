51ba197336dede4797a94a3d430850c81812a912
GIS Warrior <28575212+jvolosin@users.noreply.github.com>
2020-12-15 07:56:15 -0500

    make routes and dicts for start and startend query

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def homepage():
 
 
 @app.route("/api/v1.0/temperature")
-def station():
+def temperature():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
@@ -70,7 +70,7 @@ def station():
 
 
 @app.route("/api/v1.0/stations")
-def passengers():
+def stations():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
@@ -94,17 +94,85 @@ def passengers():
 
     session.close()
 
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
 
-    return jsonify(all_passengers)
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
+    
+    # Reproduce the precipitation query from the Station Analysis section of part 1. 
+
+    query_list = session.query(Station.station, func.count(Measurement.station)).\
+        group_by(Measurement.station).\
+        order_by(func.count(Measurement.station).desc()).all()
+    
+    station_analysis = []
+    for result in query_list
+        station_dict = {}
+        station_dict["Station"] = station
+        station_dict["Observations"] = obs_count
+        station_analysis.append(station_dict)
+       
+    return jsonify(station_analysis)
+
+    session.close()
+
+ #/prcp/<start>` and `/prcp/<start>/<end>`
+
+  #Create a query that returns the minimum precipitation, the average precipitation, and the max precipitation for a given start or start-end range.
+    #Hint: You will need to use a function such as `func.min`, `func.max`, `func.avg`, and `func.count` in your queries.
+
+  #When given the start date only, calculate min, max, and avg for all dates greater than and equal to the start date.
+
+  #When given the start and the end date, calculate the min, avg, and max for dates between the start and end date inclusive.
+  
+  #Return a JSONified dictionary of min, max, and avg temperatures.
+	#Note, the result will be a single dictionary of the format: `{"min_temp": min_temp, "max_temp": max_temp, "avg_temp": avg_temp}`
+
+@app.route("/api/v1.0/pcrp/<start")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    start_date = session.query(Station.station, func.count(Measurement.station)).\
+        group_by(Measurement.station).\
+        order_by(func.count(Measurement.station).desc()).all()
+    
+    start_precip_data = []
+    for result in start_date
+        start_dict = {}
+        start_dict["min_temp"] = min_temp
+        start_dict["max_temp"] = max_temp
+        start_dict["avg_temp"] = avg_temp
+        station_analysis.append(start_dict)
+       
+    return jsonify(start_precip_data)
+
+    session.close()
+
+
+
+@app.route("/api/v1.0/pcrp/<start>/<end")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    start_end_date = session.query(Station.station, func.count(Measurement.station)).\
+        group_by(Measurement.station).\
+        order_by(func.count(Measurement.station).desc()).all()
+    
+    start_end_precip_data = []
+    for result in start_end_date
+        start_end_dict = {}
+        start_end_dict["min_temp"] = min_temp
+        start_end_dict["max_temp"] = max_temp
+        start_end_dict["avg_temp"] = avg_temp
+        station_analysis.append(start_end_dict)
+       
+    return jsonify(start_end_precip_data)
+
+    session.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
